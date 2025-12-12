92ef893bcb3d50f5045302e48a2225c333ed4b97
dafaelpriza <elprizadafa@gmail.com>
2025-12-12 18:22:04 +0700

    update halaman utama

--- code/Halaman Utama/halaman_utama.py
+++ code/Halaman Utama/halaman_utama.py
@@ -102,24 +102,18 @@ if menu == "Home":
             "<h1 class='centered-title'>Deskripsi Kelompok</h1>", unsafe_allow_html=True
         )
         st.markdown(
-            """<div style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est 
-                    laborum.</div>""",
+            """<div style="text-align: justify;"></div>""",
             unsafe_allow_html=True,
         )
         st.write(""" """)
-        foto_kelompok = "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_"
+        foto_kelompok = "https://drive.google.com/uc?export=view&id=1Gny0GZHIDORK7yGyweU7M3jce0m_WHY_"
         layout(foto_kelompok)
         st.markdown(
-            """<div style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est 
-                    laborum.</div>""",
+            """<div style="text-align: justify;">Kami adalah Kelompok 03 - Markov, salah satu kelompok kaderisasi HMSD adyatama 2025 yang beranggotakan 12 orang. 
+            Markov berasal dari nama ilmuwan matematikawan rusia yang bernama Andrey Markov, yaitu 
+            matematika dan probabilitas yang artinya dalam kelompok ini setiap langkah ke depan bergantung 
+            pada kondisi saat ini, bukan pada masa lalu. Markov sebagai identitas kelompok kami dengan 
+            harapan dapat belajar, berkembang, dan berproses selama rangkaian kaderisasi ini.</div>""",
             unsafe_allow_html=True,
         )
         st.write(""" """)
@@ -135,69 +129,105 @@ elif menu == "About Us":
         )
         st.markdown("<h1 class='centered-title'>About Us</h1>", unsafe_allow_html=True)
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
+            "https://drive.google.com/uc?export=view&id=1Xv4oyPt6qLmwKy_qxlfqCVu2BKpUQnMx",
+            "https://drive.google.com/uc?export=view&id=1hhdcOJ5KU9WS52x3tIRY5WmG9yz1GL-L",
+            "https://drive.google.com/uc?export=view&id=1V7pS25dALvSAKE1Yr_4LV_F0-_2gfNQJ",
+            "https://drive.google.com/uc?export=view&id=1hDN2ivqf4oG6jT4Aenp4ojbPw_-BmnZx",
+            "https://drive.google.com/uc?export=view&id=1WwBPT2MXCi2CYzHl-2KMpVKuNw4HBRXF",
+            "https://drive.google.com/uc?export=view&id=1SOtVe7_1FKGv5RN8MkgdKNY18qVzjh91",
+            "https://drive.google.com/uc?export=view&id=1PA-nXAE-xtBxPIZ81uFV1qmhzadNQutF",
+            "https://drive.google.com/uc?export=view&id=1ZrBkfwJFxuIES-HKak8S6skMltHtqw0r",
+            "https://drive.google.com/uc?export=view&id=1NlOUH5R_1qwWRk2I6B_fatBwCtRMI1H2",
+            "https://drive.google.com/uc?export=view&id=1BS71hxb6MWesydP_yU0p2cL0tx6rTIzg",
+            "https://drive.google.com/uc?export=view&id=1XeVNkjmpYTmdmEZwbbPDBX_9gO1-84dY",
+            "https://drive.google.com/uc?export=view&id=1qTYdwvyoBMQEg8LSQLxU7pJ9MVC4P5K9",
         ]
         data_list = [
             {
-                "nama": "x",
+                "nama": "Hazel Mahesa Handhaka",
                 "sebagai": "Pak Lurah",
-                "nim": "122450016",
-                "fun_fact": "suka makan pedes, tapi ga suka efeknya",
-                "motto_hidup": "new semester new me",
+                "nim": "124450114",
+                "fun_fact": "ngga suka makanan berkuah",
+                "motto_hidup": "Allah tidak membebani suatu kaum melebihi batasannya",
             },
             {
-                "nama": "x",
+                "nama": "Sekar Dini Widya Putri",
                 "sebagai": "Bu Lurah",
-                "nim": "122450000",
-                "fun_fact": "nyemilin es bata",
-                "motto_hidup": "Ya apa ya",
+                "nim": "124450082",
+                "fun_fact": "suka banget mengoleksi stiker gemas",
+                "motto_hidup": "Berpikir besar dan mulai dari kecil",
             },
             {
-                "nama": "x",
+                "nama": "Dafa Elpriza",
                 "sebagai": "Anggota",
-                "nim": "122450083",
-                "fun_fact": "nyemilin es bata",
-                "motto_hidup": "mantap",
+                "nim": "124450131",
+                "fun_fact": "Ga suka durian",
+                "motto_hidup": "hidup yang tidak dipertaruhkan tidak akan pernah dimenangkan",
             },
             {
-                "nama": "x",
+                "nama": "Salavi Naharani",
                 "sebagai": "Anggotaa",
-                "nim": "122450000",
-                "fun_fact": "nyemilin",
-                "motto_hidup": "jalanin dulu aja",
+                "nim": "124450090",
+                "fun_fact": "Ga suka makan",
+                "motto_hidup": "Kehidupan adalah perjalanan, nikmati setiap prosesnya",
             },
             {
-                "nama": "x",
+                "nama": "Melva Shaprina Febrianti",
                 "sebagai": "Anggotaa",
-                "nim": "122450100",
-                "fun_fact": "nyemilin es bata, kiko",
-                "motto_hidup": "jalanin dulu keles",
+                "nim": "122450087",
+                "fun_fact": "tidur harus make kain ibu",
+                "motto_hidup": "Hari ini sulit, tapi besok bisa jadi luar biasa",
             },
             {
-                "nama": "x",
+                "nama": "Bunga Clarisa Sefa",
                 "sebagai": "Anggotaa",
-                "nim": "122450100",
-                "fun_fact": "nyemilin es bata, kiko",
-                "motto_hidup": "jalanin dulu keles",
+                "nim": "124450097 ",
+                "fun_fact": "Suka buka kulkas berulang ulang padahal udah tau isinya",
+                "motto_hidup": "Tak ada pelangi tanpa hujan, tak ada keberhasilan tanpa perjuangan",
             },
             {
-                "nama": "x",
+                "nama": "Gatfan Nadif Ali",
                 "sebagai": "Anggotaa",
-                "nim": "122450100",
-                "fun_fact": "nyemilin es bata, kiko",
-                "motto_hidup": "jalanin dulu keles",
+                "nim": "124450001",
+                "fun_fact": "Multitasking pas lagi santai",
+                "motto_hidup": "It is what it is",
             },
             {
-                "nama": "x",
+                "nama": "Bryan Paskah telaumbanua",
                 "sebagai": "Anggotaa",
-                "nim": "122450110",
-                "fun_fact": "nyemilin es bata, kiko",
-                "motto_hidup": "jalanin dulu keles",
+                "nim": "124450003",
+                "fun_fact": "Suka merendam kaki",
+                "motto_hidup": "Lakukan yang terbaik, dan tetap rendah hati",
             },
+            {
+                "nama": "Allisha",
+                "sebagai": "Anggotaa",
+                "nim": "124450019",
+                "fun_fact": "Ga suka makan ikan lele ",
+                "motto_hidup": "Hidup santai selagi masih ada hari esok",
+            },
+            {
+                "nama": "Afdal Rahmad Setiawan",
+                "sebagai": "Anggotaa",
+                "nim": "124450008",
+                "fun_fact": "Kuat makan  pedas",
+                "motto_hidup": "jika orang lain bisa kenapa kita tidak",
+            },
+            {
+                "nama": "Vannisha Ramadhani",
+                "sebagai": "Anggotaa",
+                "nim": "124450078",
+                "fun_fact": "Suka nabrak apa aja",
+                "motto_hidup": "jangan menunggu hujan reda, tetapi menarilah di tangan hujan.",
+            },
+            {
+                "nama": "Adinda Deswita Maharani",
+                "sebagai": "Anggotaa",
+                "nim": "124450083",
+                "fun_fact": "Bisa gerakin kuping",
+                "motto_hidup": "just do what u love and love what u do",
+            },
+            
         ]
         display_images_with_data(gambar_urls, data_list)
 
